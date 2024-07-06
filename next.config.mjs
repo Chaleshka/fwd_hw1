af5a4e67c2e9d39d7e711bd07733e861ee924b7c
chaleshka <52744003+Chaleshka@users.noreply.github.com>
2024-07-06 23:12:25 +0300

    .

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    basePath: '/fwd_hw1',
-    assetPrefix: '/fwd_hw1/',
-    trailingSlash: true,};
+    basePath: '/fwd_hw1', // если требуется
+    assetPrefix: '/fwd_hw1/', // если требуется
+    trailingSlash: true, // если требуется
+    output: 'export',};
 
 export default nextConfig;
